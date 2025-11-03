b2c03d90cc5e5a9c5b16876e952ca79030b23cff
Vladislav Chetrari <vlad.chetrari.work@gmail.com>
2025-11-03 22:46:52 +0100

    loading indicator

--- feature/launch/src/com/vchetrari/resume/feature/launch/LaunchScreen.kt
+++ feature/launch/src/com/vchetrari/resume/feature/launch/LaunchScreen.kt
@@ -2,12 +2,14 @@ package com.vchetrari.resume.feature.launch
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -32,6 +34,9 @@ private fun LaunchScreen(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        BasicText("Loading...")
+        //oh no there's no LoadingIndicator from material3 !!!
+        CircularProgressIndicator(
+            modifier = Modifier.size(100.dp),
+        )
     }
 }
