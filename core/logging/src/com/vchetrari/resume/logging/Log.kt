c9ca3df72ace3927bf8abc2a926cfdb3bbb1be17
Vladislav Chetrari <vlad.chetrari.work@gmail.com>
2025-11-03 22:46:36 +0100

    rm delay from log initializing

--- core/logging/src/com/vchetrari/resume/logging/Log.kt
+++ core/logging/src/com/vchetrari/resume/logging/Log.kt
@@ -2,12 +2,10 @@ package com.vchetrari.resume.logging
 
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
-import kotlinx.coroutines.delay
 
 typealias Log = Napier
 
-suspend fun Log.init() {
-    delay(1000L)
+fun Log.init() {
     //TODO remove debug logging once build types support added for Amper
     base(DebugAntilog())
     Log.d("Logging initialized!")
